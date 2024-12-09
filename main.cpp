77eab55f4eabc9f2c1fec54719ccaca51f416797
wyq <1903910367@qq.com>
2024-12-09 12:32:19 +0800

    初始化项目结构

---
+++ main.cpp
@@ -0,0 +1,10 @@
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    // QApplication a(argc, argv);
+    qDebug() << "Hello happy_elimitate" ;
+    // return a.exec();
+    return 0;
+}
