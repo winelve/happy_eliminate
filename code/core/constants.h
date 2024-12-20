6429b957114ef1e0ce125a22f5e25039c698dbe4
wyq <1903910367@qq.com>
2024-12-20 21:17:26 +0800

    历史上传,合并分支

--- code/core/constants.h
+++ code/core/constants.h
@@ -2,12 +2,12 @@
 #define CONSTANTS_H
 
 namespace Constants {
+
 const int k_board_padding = 20;
 const int k_cell_size = 64;
 const int k_duration_time = 16;
-const int ktype_size = 5;
+const int ktype_size = 6;
 const float move_speed = 400;
-
 
 const int kspecial_cube_row = 1;
 const int kspecial_cube_column = 2;
